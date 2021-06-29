042b46ff0561cb443e4cd7e9120c86b0df820fcc
有吉　貴紀 <>
2021-06-29 11:42:20 +0900

    reduceメソッドの学習完了

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,20 @@
 const arry = [1,2,3,4,5];
+
+arry.reduce(
+  function (accu, curr) {
+  console.log(accu, curr);
+    // return accu * curr;
+  }, 10);
+
+const str = "animation";
+const strArry = str.split('');
+console.log(strArry);
+
+
+// accu(前回のループで帰ってきた値) + curr(オブジェクトから受け取った値)
+const result = strArry.reduce((accu, curr) => {
+  // return accu + '<' + curr + '>';
+  return `${accu} <${curr}> `;
+}, "");
+
+console.log(result)
--- 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -1,9 +1,23 @@
 const str = 'animation';
 const strArry = str.split('');
 
+
+// 課題: 『animation』という文字列を <a><n>...と加工する
+
+
 function tag(accu, curr) {
-    return `${accu}<${curr}>`;
+  return `${accu}<${curr}>`;
 }
 
-const result = strArry.reduce(tag, "");
+// forEachで配列要素１つずつtag関数を呼び出して文字列に加工を加える
+function reduce(callBack, arry,  defaultValue) {
+  let accu = defaultValue;
+  arry.forEach(curr => {
+    accu = callBack(accu, curr);         //ここではcallBack関数の返り値をaccuとして再代入し、次の繰り返し処理で再利用している
+  })
+  return accu
+}
+
+// reduce関数を呼び出してtag関数、配列、accuの初期値となる空文字を渡す
+const result = reduce(tag, strArry, "" );
 console.log(result);
