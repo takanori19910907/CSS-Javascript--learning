29dcb7dd8f41dbed64c9cbd6980c6498e9b07405
有吉　貴紀 <>
2021-07-01 17:04:16 +0900

    実装完了

--- 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/020_スクロール×文字アニメーション/start/main.js
@@ -1,24 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // 処理①:ScrollObserverクラスオブジェクトに渡す関数を作成
+  // 監視状況(bool値)に合わせて処理が走り
+  // trueのとき(交差したとき)のみel(entry.target)のアニメーションを実行する
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TextAnimation(el);
+      ta.animate();
+    }      
+  } 
 
-  const els = document.querySelectorAll('.animate-title');
-    const cb = function(entries, observer) {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            // 文字が画面に入った時にTextAnimationを作成(文字分割を実行)
-                const ta = new TextAnimation(entry.target);
-            ta.animate();
-            // アニメーション実行後、監視を終了
-            observer.unobserve(entry.target);
-            }
-        });
-    }
-  const options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0
-  };
-  const io = new IntersectionObserver(cb, options);
-  els.forEach(
-    el => io.observe(el));
+// ②ScrollObserverクラスオブジェクトを作成(必要情報 要素名と関数を渡す)
+  const so = new ScrollObserver('.animate-title', cb);
+  // so.destroy();
 });
-
